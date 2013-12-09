f72fa5a2ecde72ebee49a098946e070f258c9484
Himanshu <himanshupandey.in@gmail.com>
2013-12-09 15:32:42 -0800

    Adding some comments to voice recognition

--- src/com/exercise/voicerecognitionexample/VoiceRecognitionActivity.java
+++ src/com/exercise/voicerecognitionexample/VoiceRecognitionActivity.java
@@ -27,8 +27,8 @@ import android.widget.Toast;
 
 public class VoiceRecognitionActivity extends Activity {
 	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1001;
- 
-	private EditText metTextHint; 
+
+	private EditText metTextHint;
 	private ListView mlvTextMatches;
 	private Spinner msTextMatches;
 	private Button mbtSpeak;
@@ -42,12 +42,11 @@ public class VoiceRecognitionActivity extends Activity {
 		msTextMatches = (Spinner) findViewById(R.id.sNoOfMatches);
 		mbtSpeak = (Button) findViewById(R.id.btSpeak);
 	}
-	
+
 	/**
 	 * Function to Check if voice recognition is present
 	 */
 	public void checkVoiceRecognition() {
-		// 
 		PackageManager pm = getPackageManager();
 		List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(
 				RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
@@ -58,21 +57,26 @@ public class VoiceRecognitionActivity extends Activity {
 		}
 	}
 
+	/**
+	 * This function calls the intent to handle voice recognition
+	 * 
+	 * @param view
+	 */
 	public void speak(View view) {
 		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-		// Specify the calling package to identify your application
+		/* Specify the calling package to identify your application */
 		intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass()
 				.getPackage().getName());
 
-		// Display an hint to the user about what he should say.
+		/* Display an hint to the user about what he should say. */
 		intent.putExtra(RecognizerIntent.EXTRA_PROMPT, metTextHint.getText()
 				.toString());
 
-		// Given an hint to the recognizer about what the user is going to say
+		/* Given an hint to the recognizer about what the user is going to say */
 		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
 				RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
 
-		// If number of Matches is not selected then return show toast message
+		/* If number of Matches is not selected then return show toast message */
 		if (msTextMatches.getSelectedItemPosition() == AdapterView.INVALID_POSITION) {
 			Toast.makeText(this, "Please select No. of Matches from spinner",
 					Toast.LENGTH_SHORT).show();
@@ -81,9 +85,10 @@ public class VoiceRecognitionActivity extends Activity {
 
 		int noOfMatches = Integer.parseInt(msTextMatches.getSelectedItem()
 				.toString());
-		// Specify how many results you want to receive. The results will be
-		// sorted where the first result is the one with higher confidence.
-
+		/*
+		 * Specify how many results you want to receive. The results will be
+		 * sorted where the first result is the one with higher confidence.
+		 */
 		intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, noOfMatches);
 
 		startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
@@ -93,57 +98,55 @@ public class VoiceRecognitionActivity extends Activity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == VOICE_RECOGNITION_REQUEST_CODE)
 
-			//If Voice recognition is successful then it returns RESULT_OK
-			if(resultCode == RESULT_OK) {
-				
-				
+			/* If Voice recognition is successful then it returns RESULT_OK */
+			if (resultCode == RESULT_OK) {
 
 				ArrayList<String> textMatchList = data
-				.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+						.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 
-				
 				String tempResult = textMatchList.get(0);
 				System.out.println("found: " + tempResult);
-				
-				if (tempResult.equalsIgnoreCase("haha")){
-					Intent myIntent = new Intent(VoiceRecognitionActivity.this, AndroidCamera.class);
+
+				if (tempResult.equalsIgnoreCase("haha")) {
+					Intent myIntent = new Intent(VoiceRecognitionActivity.this,
+							AndroidCamera.class);
 					startActivity(myIntent);
 				}
-				
+
 				if (!textMatchList.isEmpty()) {
-					// If first Match contains the 'search' word
-					// Then start web search.
+					/* If first Match contains the 'search' word
+					Then start web search. */
 					if (textMatchList.get(0).contains("search")) {
 
-						String searchQuery = textMatchList.get(0).replace("search",
-						" ");
+						String searchQuery = textMatchList.get(0).replace(
+								"search", " ");
 						Intent search = new Intent(Intent.ACTION_WEB_SEARCH);
 						search.putExtra(SearchManager.QUERY, searchQuery);
 						startActivity(search);
 					} else {
-						// populate the Matches
-						mlvTextMatches
-						.setAdapter(new ArrayAdapter<String>(this,
-								android.R.layout.simple_list_item_1,
+						/* populate the Matches */
+						mlvTextMatches.setAdapter(new ArrayAdapter<String>(
+								this, android.R.layout.simple_list_item_1,
 								textMatchList));
 					}
 
 				}
-			//Result code for various error.	
-			}else if(resultCode == RecognizerIntent.RESULT_AUDIO_ERROR){
+				/* Result code for various error. */
+			} else if (resultCode == RecognizerIntent.RESULT_AUDIO_ERROR) {
 				showToastMessage("Audio Error");
-			}else if(resultCode == RecognizerIntent.RESULT_CLIENT_ERROR){
+			} else if (resultCode == RecognizerIntent.RESULT_CLIENT_ERROR) {
 				showToastMessage("Client Error");
-			}else if(resultCode == RecognizerIntent.RESULT_NETWORK_ERROR){
+			} else if (resultCode == RecognizerIntent.RESULT_NETWORK_ERROR) {
 				showToastMessage("Network Error");
-			}else if(resultCode == RecognizerIntent.RESULT_NO_MATCH){
+			} else if (resultCode == RecognizerIntent.RESULT_NO_MATCH) {
 				showToastMessage("No Match");
-			}else if(resultCode == RecognizerIntent.RESULT_SERVER_ERROR){
+			} else if (resultCode == RecognizerIntent.RESULT_SERVER_ERROR) {
 				showToastMessage("Server Error");
 			}
 		super.onActivityResult(requestCode, resultCode, data);
 	}
-	void showToastMessage(String message){
+
+	void showToastMessage(String message) {
 		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 	}
 }
